f46f200921b6e79869006337948868e5f2559d0e
amanrj03 <infineamanrj1710@gmail.com>
2025-04-12 02:49:28 +0530

    added skeleton route

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+
+
+app.post("/user/signup", function(req, res){
+    
+})
+
+app.post("/user/signin", function(req, res){
+
+})
+
+app.post("/coure/puchase", function(req, res){
+
+})
+
+app.get("/user/purchases", function(req, res){
+
+})
+
+app.post("/courses", function(req, res){
+
+})
+
+app.listen(3000);
